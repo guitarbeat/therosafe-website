ff042d6ca0149a629c924a726fc38523b0ec2875
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-04-03 13:11:53 -0500

    some changes

--- js/components/Welcome.js
+++ js/components/Welcome.js
@@ -1,11 +1,11 @@
 const WelcomeSection = () => {
   return (
     <section id="welcome">
-      <p>I'm Jesse and I'm a frontend dev.</p>
-      <img src="./css/img/wave.png" />
-      <h1>hello.</h1>
+      <p>Unlock the Power of Theranostics with Therosafe.</p>
+      <img src="./css/img/therosafe.png" />
+      <h1>Therosafe</h1>
     </section>
   );
-}
+};
 
 export default WelcomeSection;
